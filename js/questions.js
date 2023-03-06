65bb225928ee56a09de09ba00b2c344d55994312
ridwan <119526813+rdpgz18@users.noreply.github.com>
2023-03-06 19:51:50 +0700

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,128 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Baterai adalah sumber listrik yang praktis. Hal itu disebabkan karena baterai?",
+    answer: "b. Mudah dibawa kemana-mana",
+    options: [
+      "a. Harganya mahal",
+      "b. Mudah dibawa kemana-mana",
+      "c. Susah dibawa kemana-mana",
+      "d. Terlalu besar"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Bola yang di tendang ke permukaan tanah, maka suatu saat bola akan berhenti. Hal itu disebabkan oleh?",
+    answer: "d. Elastis",
+    options: [
+      "a. Ringan",
+      "b. Keras",
+      "c. Kuat",
+      "d. Elastis"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Perpindahan panas yang diikuti dengan aliran zatnya dinamakan?",
+    answer: "d. konveksi",
+    options: [
+      "a. Respirasi",
+      "b. Radiasi",
+      "c. Konduksi",
+      "d. konveksi"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Berikut ini ialah contoh-contoh perpindahan panas secara radiasi, kecuali?",
+    answer: "c. Air panas yang mendidih",
+    options: [
+      "a. Orang-orang yang merasakan kehangatan di sekitar api unggun",
+      "b. Panasnya api lilin yang terasa di dekatnya",
+      "c. Air panas yang mendidih",
+      "d. Cahaya matahari sampai ke bumi"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Kayu, kertas dan kain ialah contoh dari benda-benda?",
+    answer: "b. Isolator panas",
+    options: [
+      "a. Mudah memuai",
+      "b. Isolator panas",
+      "c. Konduktor panas",
+      "d. Mudah panas"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Cangkir yang diisi dengan air panas maka gagangnya ikut terasa panas juga. Hal ini menujukkan bahwa adanya perpindahan panas secara?",
+    answer: "d. Konduksi",
+    options: [
+      "a. Radiasi",
+      "b. Konveksi",
+      "c. Kondensasi",
+      "d. Konduksi"
+    ]
+  },
+  {
+  numb: 7,
+    question: "Taman Nasional yang memiliki keindahan pemandangan bawah lautnya ialah?",
+    answer: "d. Bunaken",
+    options: [
+      "a. Ujung kulon",
+      "b. Alas purwo",
+      "c. Gunung Merapi",
+      "d. Bunaken"
+    ]
+  },
+  {
+  numb: 8,
+    question: "Egi dan teman-teman bermain permainan bola kasti. disaat itu Egi siap untuk memukul bola. Bola yang segera datang ke arahnya tepat dipukul kembali sehingga bola itu memantul kembali. Gaya yang dilakukan dari egi akan menyebabkan bola?",
+    answer: "b. Berubah bentuk",
+    options: [
+      "a. Bergerak menjadi diam",
+      "b. Berubah bentuk",
+      "c. Berubah arah",
+      "d. Bergerak"
+    ]
+  },
+  {
+  numb: 9,
+    question: "Singkong pada umumnya dikembangbiakkan dengan cara?",
+    answer: "a. Setek",
+    options: [
+      "a. Setek",
+      "b. Umbi",
+      "c. Biji",
+      "d. Tunas"
+    ]
+  },
+  {
+  numb: 10,
+    question: "planet yang dijuluki bintang kejora yaitu?",
+    answer: "c. Venus",
+    options: [
+      "a. Mars",
+      "b. Jupiter",
+      "c. Venus",
+      "d. Bulan"
+    ]
+  },
+  
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
